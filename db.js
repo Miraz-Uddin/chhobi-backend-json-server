1466904a7097103f3bf8c133dbcea0ff65b260a2
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-04-20 02:27:09 +0600

    User Table exported

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const movies = require("./database/movies");
 const genres = require("./database/genres");
+const users = require("./database/users");
 
 module.exports = {
   movies,
   genres,
+  users
 };
